e3f2784aaa43c98b744142cd28071e3d1506a21e
michael.rode <michael.rode@rigup.com>
2020-03-07 22:34:36 -0600

    remove mongoose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose'; // add this
 import { AudioBayModule } from './audiobay/audiobay.module';
 import { TransmissionModule } from './transmission/transmission.module';
 import { GdriveModule } from './gdrive/gdrive.module';
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-blog-project', {
-      useNewUrlParser: true,
-    }),
-    AudioBayModule,
-    TransmissionModule,
-    GdriveModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AudioBayModule, TransmissionModule, GdriveModule],
 })
 export class AppModule {}
